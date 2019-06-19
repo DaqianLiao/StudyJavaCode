cfb8d9641dbb51da6d11866889aa99e345be063b
diligent_leo <360989637@qq.com>
2019-06-19 14:00:28 +0800

    hello world

---
+++ src/main/java/com/ldq/study/App.java
@@ -0,0 +1,13 @@
+package com.ldq.study;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * Hello world!
+ */
+class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
