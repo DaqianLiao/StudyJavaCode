c8cd7609146968e3134a499ef977e93939e0a6d9
diligent_leo <360989637@qq.com>
2019-06-27 13:59:56 +0800

    add string equals demo

---
+++ src/main/java/com/ldq/study/StringDemo/EqualsDemo.java
@@ -0,0 +1,22 @@
+package com.ldq.study.StringDemo;
+
+public class EqualsDemo {
+
+    public static void testEqual(){
+//        ==判断是否相等
+        String str1 = "hello";
+        String str2 = "hello";
+//        true 因为两个地址相同，都存在与常量池中
+        System.out.println(str1 == str2);
+        String str3 = new String("hello");
+//        false 因为str3是在堆中新创建的对象，和str1地址不一样
+        System.out.println(str1 == str3);
+
+//       查看源码可知两者先比较地址是否相同，后比较值是否相同
+        System.out.println(str1.equals(str3));//true
+    }
+
+    public static void main(String[] args) {
+        testEqual();
+    }
+}
